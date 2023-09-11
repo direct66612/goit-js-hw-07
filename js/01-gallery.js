5dcbc3e55b1c11d02b1690c413c7d37a991f26c8
direct66612 <direct66612@gmail.com>
2023-09-11 15:00:51 -0400

    completed task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,29 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-
-console.log(galleryItems);
+const list = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(({ preview, original, description }) => {
+    return `<li class="gallery__item"><a class="gallery__link" href="${original}">
+  <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a></li>`;
+  })
+  .join("");
+list.insertAdjacentHTML("beforeend", markup);
+list.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  const currentProduct = e.target.closest(".gallery__image");
+  const product = galleryItems.find(
+    ({ original }) => original === currentProduct.dataset.source
+  );
+  const instance = basicLightbox.create(
+    `<div class="modal"><img src="${product.original}" alt="${product.description}"/></div>`
+  );
+  instance.show();
+  window.addEventListener("keydown", (e) => {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+  });
+});
