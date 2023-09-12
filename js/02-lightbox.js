54d6da707e8a649bd39884815a657862f570d45b
direct66612 <direct66612@gmail.com>
2023-09-12 00:09:38 -0400

    completed task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
+const list = document.querySelector(".gallery");
 // Change code below this line
-
-console.log(galleryItems);
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}"/></a></li>`
+  )
+  .join("");
+list.insertAdjacentHTML("beforeend", markup);
+list.addEventListener("click", handleClick);
+function handleClick(event) {
+  event.preventDefault();
+  if (event.target === event.currentTarget) {
+    return;
+  }
+}
+const instance = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
+instance.on("show.simpleLightbox");
